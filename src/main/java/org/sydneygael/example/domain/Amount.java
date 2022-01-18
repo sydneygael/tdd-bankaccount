d8fe3c7c875cd68d951cfcd1c52bfbfb84bd6320
sydney.adjou-moumouni <sydgael@gmail.com>
2022-01-18 23:34:13 +0100

    US1 : Deposit Cannot be negative

--- src/main/java/org/sydneygael/example/domain/Amount.java
+++ src/main/java/org/sydneygael/example/domain/Amount.java
@@ -1,6 +1,13 @@
 package org.sydneygael.example.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record Amount (BigDecimal value){
+
+    public Amount{
+        Objects.requireNonNull(value);
+        if(value.signum()==-1)
+            throw new IllegalArgumentException("Amount must not be negative");
+    }
 }
--- src/test/java/org/sydneygael/example/domain/AccountTest.java
+++ src/test/java/org/sydneygael/example/domain/AccountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AccountTest {
 
@@ -19,6 +20,15 @@ class AccountTest {
         Account account = new Account(new Balance(BigDecimal.ZERO));
         account.deposit(new Amount(BigDecimal.TEN));
         assertThat(account.balance()).isEqualTo(Balance.of(BigDecimal.TEN));
+    }
+
+    @Test
+    void should_new_account_throws_execption_when_deposit_an_amount_of_negative_amount() {
+        assertThatThrownBy(() -> {
+            Account account = new Account(new Balance(BigDecimal.ZERO));
+            account.deposit(new Amount(BigDecimal.valueOf(10).negate()));
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
